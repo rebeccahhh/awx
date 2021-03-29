f17bdc1534cf4a73994ac2733ca7694efe6ab953
Rebeccah <rhunter@redhat.com>
2021-03-29 12:13:11 -0400

    restructure to account for users not filling in credential data

--- awx/main/tasks.py
+++ awx/main/tasks.py
@@ -907,18 +907,20 @@ class BaseTask(object):
         }
 
         if instance.execution_environment.credential:
-            path = self.build_private_data_dir(instance)
-            with open(path + '/auth.json', 'w') as authfile:
-                # do sanitation for this with  instance.execution_environment.credential.inputs
-                host = instance.execution_environment.credential.get_input('host')
-                username = instance.execution_environment.credential.get_input('username')
-                password = instance.execution_environment.credential.get_input('password')
-                token = "{}:{}".format(username, password)
-                auth_data = {'auths': {host: {'auth': b64encode(token.encode('ascii')).decode()}}}
-                authfile.write(json.dumps(auth_data, indent=4))
-            authfile.close()
-            os.chmod(authfile.name, stat.S_IRUSR | stat.S_IWUSR | stat.S_IXUSR)
-            params["container_options"].append(f'--authfile={authfile.name}')
+            if ('host', 'username', 'password') in instance.execution_environment.credential.inputs:
+                path = self.build_private_data_dir(instance)
+                with open(path + '/auth.json', 'w') as authfile:
+                    host = instance.execution_environment.credential.get_input('host')
+                    username = instance.execution_environment.credential.get_input('username')
+                    password = instance.execution_environment.credential.get_input('password')
+                    token = "{}:{}".format(username, password)
+                    auth_data = {'auths': {host: {'auth': b64encode(token.encode('ascii')).decode()}}}
+                    authfile.write(json.dumps(auth_data, indent=4))
+                authfile.close()
+                os.chmod(authfile.name, stat.S_IRUSR | stat.S_IWUSR | stat.S_IXUSR)
+                params["container_options"].append(f'--authfile={authfile.name}')
+            else:
+                logger.exception('Please recheck that your host, username, and password fields are all filled.')
 
         pull = instance.execution_environment.pull
         if pull:
