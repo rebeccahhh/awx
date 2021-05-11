cfd9449a853794e47cb6438419bef64f01d2eb31
Shane McDonald <me@shanemcd.com>
2021-05-11 18:19:16 -0400

    Usability tweaks
    - Uses a positional argument instead of a named arg
    - More helpful output

--- awx/main/management/commands/export_custom_venv.py
+++ awx/main/management/commands/export_custom_venv.py
@@ -1,8 +1,10 @@
 # Copyright (c) 2021 Ansible, Inc.
 # All Rights Reserved
+import sys
 
 from awx.main.utils.common import get_custom_venv_choices, get_custom_venv_pip_freeze
 from django.core.management.base import BaseCommand
+from django.conf import settings
 
 
 class Command(BaseCommand):
@@ -10,9 +12,9 @@ class Command(BaseCommand):
 
     def add_arguments(self, parser):
         parser.add_argument(
-            '--path',
-            dest='path',
+            'path',
             type=str,
+            nargs='?',
             default='',
             help='run without arguments to see a list of paths, run with one of those paths as an argument and see the pip freeze data',
         )
@@ -21,8 +23,25 @@ class Command(BaseCommand):
         super(Command, self).__init__()
         if options.get('path'):
             pip_data = get_custom_venv_pip_freeze(options.get('path'))
-            print(pip_data)
+            if pip_data:
+                print(pip_data)
         else:
             venvs = get_custom_venv_choices()
-            for venv in venvs:
-                print(venv)
+            if venvs:
+                print('# {}'.format("Discovered virtual environments:"))
+                for venv in venvs:
+                    print(venv)
+
+                msg = [
+                    '',
+                    'To export the contents of a virtual environment, ' 're-run while supplying the path as an argument:',
+                    'awx-manage export_custom_venv /path/to/venv',
+                ]
+                print('\n'.join(msg))
+            else:
+                msg = ["No custom virtual environments detected in:", settings.BASE_VENV_PATH]
+
+                for path in settings.CUSTOM_VENV_PATHS:
+                    msg.append(path)
+
+                print('\n'.join(msg), file=sys.stderr)
